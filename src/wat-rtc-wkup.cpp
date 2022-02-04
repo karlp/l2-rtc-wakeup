1905e4bf0448e4baf29abcfb4ecc6bdda00e02d2
Karl Palsson <karlp@tweak.net.au>
2022-02-04 22:29:08 +0000

    wat-rtc-wkup: make l4 mostly work
    We need an actual irq handler, which kinda irks me, but see section
    14.3.2 "Wakeup event management" and following.

--- src/wat-rtc-wkup.cpp
+++ src/wat-rtc-wkup.cpp
@@ -19,13 +19,23 @@
 #include <syscfg/syscfg.h>
 #include <uart/uart.h>
 
+#if defined(STM32WB)
 auto led_r = GPIOB[1];
 auto led_g = GPIOB[0];
 auto led_b = GPIOB[5];
+auto led = led_g;
 auto sw_1 = GPIOC[4];
 auto sw_2 = GPIOD[0];
 auto sw_3 = GPIOD[1];
-auto led = led_g;
+auto exti_rtc_wkup = 19;
+#elif defined(STM32L4)
+auto led_b = GPIOA[5];
+auto sw_3 = GPIOC[13];
+auto led = led_b;
+auto exti_rtc_wkup = 20;
+#else
+#error "Unhandled platform"
+#endif
 
 /* disable this with a debugger if you want to measure power sanely
  * You'll then have to watch the ITM channels to see that things do what you think.
@@ -43,9 +53,9 @@ extern "C" int _write(int file, char* ptr, int len) {
         if (file == STDOUT_FILENO || file == STDERR_FILENO) {
                 for (i = 0; i < len; i++) {
                         if (ptr[i] == '\n') {
-				USART1.write_blocking(ptr[i]);
+				LPUART1.write_blocking('\r');
                         }
-		USART1.write_blocking(ptr[i]);
+		LPUART1.write_blocking(ptr[i]);
                 }
                 return i;
         }
@@ -54,11 +64,34 @@ extern "C" int _write(int file, char* ptr, int len) {
 }
 
 
+static inline uint32_t __get_PRIMASK(void)
+{
+  uint32_t result;
+
+  __asm volatile ("MRS %0, primask" : "=r" (result) :: "memory");
+  return(result);
+}
+
+static inline void __set_PRIMASK(uint32_t priMask)
+{
+  __asm volatile ("MSR primask, %0" : : "r" (priMask) : "memory");
+}
+
+static inline void __disable_irq(void)
+{
+  __asm volatile ("cpsid i" : : : "memory");
+}
+
+
+
+
+
 /**
  * Run at 32Mhz instead of laks "top speed" by default.
  * 32Mhz is what many ST WB demos run at, and if you don't need more, it's
  * probably a good choice.
  */
+#if defined(STM32WB)
 void krcc_init32(void) {
 	// Prefetch and both caches, plus 1WS for 32MHz
 	FLASH->ACR = 0x700 | 1;
@@ -72,54 +105,92 @@ void krcc_init32(void) {
 	// Make sure to clear bits, we may be waking up!
 	RCC->CFGR &= ~0x3;
 	RCC->CFGR |= 0x2;
-	while ((RCC->CFGR & (2<<2)) != (2<<2)); // SWS = HSE
+	while ((RCC->CFGR & (3<<2)) != (2<<2)); // SWS = HSE
 	// Leave prescalers alone...
 }
+#endif
+
+#if defined(STM32L4)
+static void krcc_init32(void) {
+	// We're just goign to use MSI at 32Mhz, as it's "easy" to setup,
+	// and it doesn't matter what your crystal is.  (Even if nucleos have 8Mhz)
 
 
-static void krtc_init(void) {
-	PWR->CR1 |= (1<<8); // Unlock backup domain
-	
-	// For _now_ we're doing a hard backup domain reset, so we can be sure shit works.
-	// in real world, you almost definitely don't want to do that, as you'll trash your RTC that carefully
-	// stayed running while you were stopped. ST uses the "Is LSE on?"
-	// heuristic, but you could also use a backup register as a flag.
-	bool opt_hard_reset_backup_domain = true;
-	if (opt_hard_reset_backup_domain) {
-		printf("hard reset backup domain\n");
-		RCC->BDCR |= (1<<16);
-		RCC->BDCR &= ~(1<<16);
+	// Prefetch and both caches, plus 1WS for 32MHz
+	FLASH->ACR = 0x700 | 1;
+
+	// Enable MSI (it should already be on, it's the reset clock)
+	RCC->CR |= (1<<3) | 1; // Also enable range selection here
+	// Can't modify MSI range when it's on and not ready
+	while (!(RCC->CR & (1 << 1)));
+	uint32_t msi_range = 10; // 32Mhz
+	RCC->CR &= ~(0xf<<4);
+	RCC->CR |= (msi_range << 4);
+
+	RCC->CFGR &= ~(0x3<<0); // Set MSI as clock source
+	// L4 Needs these to be enabled first.
+	RCC.enable(rcc::PWR);
+	RCC.enable(rcc::SYSCFG);
+}
+#endif
+
+
+
+static void krtc_init(void)
+{
+	// always need that even if RTC itself is already setup
+	PWR->CR1 |= (1 << 8); // Unlock backup domain
+
+	// Use the last backup register as a flag that we're already initialized
+	if (RTC->BKP[19] == 0xcafe) {
+		printf("RTC: already setup!\n");
+		return;
 	}
-	
+
+	// We've decided that it's "not setup" so go ahead and hard reset
+	// The RTC.  You want to be careful with this, or you lose your
+	// precious calendar.
+	printf("hard reset backup domain\n");
+	RCC->BDCR |= (1 << 16);
+	RCC->BDCR &= ~(1 << 16);
+
 	// make sure LSE is on, we need that for most sane uses of RTC
 	RCC->BDCR |= 1;
-	while (!(RCC->BDCR & (1<<1))) {
+	while (!(RCC->BDCR & (1 << 1))) {
 		; // wait for LSERDY
 	}
-	
-	RCC->BDCR |= (1<<8); // RTCSEL == LSE
-	RCC->BDCR |= (1<<15); // RTCEN
+
+	RCC->BDCR |= (1 << 8); // RTCSEL == LSE
+	RCC->BDCR |= (1 << 15); // RTCEN
 	RCC.enable(rcc::RTCAPB);
-	while (!(RCC->APB1ENR1 & rcc::RTCAPB)) {
-		; // make sure we have access!
-	}
-	
+	// WB simply does a single read here, and L4 just hangs
+//	while (!(RCC->APB1ENR1 & rcc::RTCAPB)) {
+//		; // make sure we have access!
+//	}
+
 	RTC.unlock();
+
 	RTC->PRER = 0x7f00ff; // default, gives 1sec from 32768
-	
+
+	// setup calendar as well
+	RTC->ISR = (1 << 7);
+	while (!(RTC->ISR & (1 << 6))) {
+		; // Wait for INITF
+	}
+	RTC->CR &= ~(1 << 6); // 24hr format.
+	RTC->DR = 0x220127 | (4 << 13); // Thursday,2022-01-27.
+	RTC->TR = 0x092042; // 09:20:42 AM
+	RTC->ISR &= ~(1 << 7); // Clear init, start calendar
+
 	// set wakekup clock sel to rtc/16, so ~488usecs per tick
-	RTC->CR &= ~(0x7<<0);
-	RTC->CR |= (0<<0);
+	RTC->CR &= ~(0x7 << 0);
+	RTC->CR |= (0 << 0);
 
 	RTC.lock();
-	
-	// Set it up now, but we will turn it on later.
-	// RTC Wakeup is via exti 19! only listed in the nvic section
-	EXTI->RTSR1 |= (1<<19);
-	EXTI->IMR1 |= (1 << 19);
-	NVIC.enable(interrupt::irq::RTC_WKUP);
 
+	RTC->BKP[19] = 0xcafe;
 }
+
 
 static void kwkup_stop(void) {
 	RTC.unlock();
@@ -129,6 +200,15 @@ static void kwkup_stop(void) {
 
 static void kwkup_start(uint16_t wu_ticks)
 {
+	// Set it up now, but we will turn it on later.
+	// RTC Wakeup is via exti 19! only listed in the nvic section
+	// l4 says rtc wakeup is exti line 20!  we know it's 19 on wb
+	EXTI->RTSR1 |= (1 << exti_rtc_wkup);
+	EXTI->IMR1 |= (1 << exti_rtc_wkup);
+	// we want to wakeup, but we don't need a handler, but I can't get it to work without?
+	NVIC.enable(interrupt::irq::RTC_WKUP);
+
+
 	RTC.unlock();
 	
 	// Disable wakeup
@@ -150,11 +230,19 @@ static void kwkup_start(uint16_t wu_ticks)
 	RTC->ISR = ~(1<<10); // Clear WUTF. (other bits are protected)
 
 	PWR->CR3 |= (1<<15); // EIWUL enable wakeup cpu1
+
+
 }
 
 static void kexti_init(void) {
 	// sw_3.. is D1...
+#if defined(STM32WB)
 	RCC.enable(rcc::GPIOD);
+#elif defined(STM32L4)
+	RCC.enable(rcc::GPIOC);
+#else
+#error "Unhandled platform, check your switch/led ports"
+#endif
 
 	sw_3.set_mode(Pin::Mode::Input);
 	sw_3.set_pull(Pin::Pull::PullUp);
@@ -165,21 +253,133 @@ static void kexti_init(void) {
 	EXTI->IMR1 |= (1 << sw_3.n); // unmask
 	EXTI->FTSR1 |= (1 << sw_3.n); // falling edge
 	
+#if defined(STM32WB)
 	NVIC.enable(interrupt::irq::EXTI1);
+#elif defined(STM32L4)
+	NVIC.enable(interrupt::irq::EXTI15_10);
+#endif
 }
+
+// Used for just plain sleep
+class HandleNull {
+public:
+	void pre(void) {};
+	void post(void) {};
+};
+
+/**
+ * Can actually handle stop modes 0,1,2, standby, and stop.
+ */
+class HandleStop {
+private:
+	uint32_t _mode;
+	uint32_t _rcc_cr, _rcc_cfgr;
+public:
+	HandleStop(uint32_t mode): _mode{mode} {};
+
+	void pre(void) {
+//		printf("stop pre %lu\n", _mode);
+		_rcc_cr = RCC->CR;
+		_rcc_cfgr = RCC->CFGR;
+		PWR.set_lpms(_mode);
+//		PWR.set_lpms_c2(_mode);
+//		if ((PWR->EXTSCR >> 8) & 0xf) {
+//			// if any of thos fucking flags is set, clear them all!
+//			PWR->EXTSCR |= 0x3;
+//		}
+		// Enable SLEEPDEEP
+		SCB->SCR |= (1<<2);
+	}
+	void post(void) {
+//		printf("stop post\n");
+		// 3. We wakeup with either HSI16 or MSI depending on RCC->CFGR:STOPWUCK...
+		// we're just going to to with "restore RCC->CR and RCC-CFGR....
+		RCC->CR = _rcc_cr;
+		RCC->CFGR = _rcc_cfgr;
+	}
+};
+
+/**
+ * Handle clocking down to allow lprun / lp sleep
+ */
+class HandleLP {
+private:
+	uint32_t _msi_range;
+	uint32_t _rcc_cr, _rcc_cfgr;
+public:
+	HandleLP(uint32_t msi_range): _msi_range{msi_range} {};
+
+	void pre(void) {
+		_rcc_cr = RCC->CR;
+		_rcc_cfgr = RCC->CFGR;
+		// Ensure MSI is on, and switch down to that range...
+		RCC->CR |= (1<<3) | 1; // Also enable range selection here
+		// Can't modify MSI range when it's on and not ready
+		while (!(RCC->CR & (1 << 1)));
+		RCC->CR &= ~(0xf<<4);
+		RCC->CR |= (_msi_range << 4);
+
+		RCC->CFGR &= ~(0x3<<0); // Set MSI as clock source
+		// SLEEPDEEP must be clear
+		SCB->SCR &= ~(1<<2);
+
+		PWR->CR1 |= (1<<14); // Set regulator low power mode
+	}
+
+	void post(void) {
+		// Restore regulator power
+		PWR->CR1 &= ~(1<<14);
+		while (PWR->SR2 & (1<<9)); // Wait for REGLPF
+
+		// restore system clock settings
+		RCC->CR = _rcc_cr;
+		RCC->CFGR = _rcc_cfgr;
+	}
+};
 
 // for starters...
+//auto handler = HandleStop(4);
+//auto handler = HandleNull();
+auto handler = HandleLP(5); // 0=100kHz, 1=200, 2=400,3=800,4=1M, 5=2M
 static void ksleep() {
-		PWR->CR3 |= (1<<15);
-		PWR.set_lpms(3);
-		PWR.set_lpms_c2(3);
+//	// Ensure that EWUIL is set
+//		PWR->CR3 |= (1<<15);
+//		PWR.set_lpms(2);
+//		PWR.set_lpms_c2(2);
+//
+//		SCB->SCR |= (1<<2); // deepsleep
 
-		SCB->SCR |= (1<<2); // deepsleep
+	kwkup_start(2);
+	handler.pre();
 
-	kwkup_start(9999);
-	
+// HEY KARL YOU EEDIOT! READ THE DOCS! YOU REALY NEED A CRIT HERE!
+//PRIMASK = 1; // Mask all interrupts (enter critical section)
+//PWR_EnterStopMode()
+//WFI
+//PWR_ExitStopMode()
+//PRIMASK = 0; // Unmask all interrupts (exit critical section)
+
 	asm volatile ("wfi");
 }
+
+static void ksleep_exit() {
+	handler.post();
+	kwkup_stop();
+}
+
+static void ksleep2(void) {
+	kwkup_start(24000);
+
+	uint32_t primask_bit = __get_PRIMASK( );
+	__disable_irq( );
+	handler.pre();
+	__asm volatile ("wfi");
+	handler.post();
+	__set_PRIMASK(primask_bit);
+
+	kwkup_stop();
+}
+
 
 static void print_date(void) {
 	uint32_t s = RTC->SSR;
@@ -197,7 +397,7 @@ static void print_date(void) {
 }
 
 static void klpuart_init(void) {
-	RCC.enable(rcc::USART1);
+//	RCC.enable(rcc::USART1);
 	RCC.enable(rcc::LPUART1);
 
 	// sysclock? lpuart1
@@ -207,34 +407,41 @@ static void klpuart_init(void) {
 	RCC->CCIPR &= ~(0x3<<0);
 	RCC->CCIPR |= (1<<0);
 
+
+#if defined(STM32WB)
 	// PA2,3 for lpuart1
 	RCC.enable(rcc::GPIOA);
-	auto pa2 = GPIOA[2];
-	auto pa3 = GPIOA[3];
-	pa2.set_af(8);
-	pa3.set_af(8);
-	pa2.set_mode(Pin::Mode::AF);
-	pa3.set_mode(Pin::Mode::AF);
+	auto lp1_rx = GPIOA[2];
+	auto lp1_tx = GPIOA[3];
+#elif defined(STM32L4)
+	RCC.enable(rcc::GPIOC);
+	auto lp1_rx = GPIOC[0];
+	auto lp1_tx = GPIOC[1];
+#endif
+	lp1_rx.set_af(8);
+	lp1_tx.set_af(8);
+	lp1_rx.set_mode(Pin::Mode::AF);
+	lp1_tx.set_mode(Pin::Mode::AF);
 
-	RCC.enable(rcc::GPIOB);
-	auto pb6 = GPIOB[6];
-	auto pb7 = GPIOB[7];
-	pb6.set_af(7);
-	pb7.set_af(7);
-	pb6.set_mode(Pin::Mode::AF);
-	pb7.set_mode(Pin::Mode::AF);
+//	RCC.enable(rcc::GPIOB);
+//	auto pb6 = GPIOB[6];
+//	auto pb7 = GPIOB[7];
+//	pb6.set_af(7);
+//	pb7.set_af(7);
+//	pb6.set_mode(Pin::Mode::AF);
+//	pb7.set_mode(Pin::Mode::AF);
+//
+//	USART1->CR1 = (1<<29); // fifo, 8bit, no parity, no special
+//	//USART1->CR1 = 0;
+//	USART1->CR2 = 0; // standard
+//	// this is for lpuart USART1->BRR = (256*32000000u / 115200); // 71111?
+//	USART1->BRR = 32000000u / 115200u;
+//	USART1->CR1 |= (1<<3)|1; // TX only, enabled
 
-	USART1->CR1 = (1<<29); // fifo, 8bit, no parity, no special
-	//USART1->CR1 = 0;
-	USART1->CR2 = 0; // standard
-	// this is for lpuart USART1->BRR = (256*32000000u / 115200); // 71111?
-	USART1->BRR = 32000000u / 115200u;
-	USART1->CR1 |= (1<<3)|1; // TX only, enabled
-
-	LPUART1->CR1 = (1<<29);
+	LPUART1->CR1 = (1<<29); // fifo, 8bit, no parity, no special
 	//LPUART1->CR1 = 0;
 	LPUART1->CR2 = 0; // standard
-	LPUART1->BRR = (256*32000000u / 115200); // 71111?
+	LPUART1->BRR = (32000000u / 115200) * 256; // careful with overflows!
 	LPUART1->CR1 |= (1<<3)|1; // TX only, enabled
 
 }
@@ -257,6 +464,7 @@ int main() {
 	
 
 	if (opt_really_use_leds) {
+		RCC.enable(rcc::GPIOA);
 		RCC.enable(rcc::GPIOB);
 		led.set_mode(Pin::Output);
 	}
@@ -272,41 +480,56 @@ int main() {
 			if (opt_really_use_leds) {
 				led.toggle();
 			}
-			USART1.write_blocking('a' + k++);
+//			USART1.write_blocking('a' + k++);
 			LPUART1.write_blocking('a' + k++);
-			if (k > 26) {
+			if (k >= 26) {
 				k = 0;
-				USART1.write_blocking('\n');
+//				USART1.write_blocking('\n');
 				LPUART1.write_blocking('\n');
+				print_date();
 			}
 		}
 		// Ok, button pressed, go to sleep!
 		printf("going to sleep at: ");
 		print_date();
 		led.off();
-		ksleep();
+
+//		uint32_t primask_bit = __get_PRIMASK( );
+//		__disable_irq( );
+//		ksleep();
+//		ksleep_exit();
+//		__set_PRIMASK(primask_bit);
+		ksleep2();
 		pressed = false;
 		printf("woke up at.. ");
 		print_date();
-		kwkup_stop();
 	}
 		
 	return 0;
 }
 
 
+// We shouldn't need an irq, we're just using it as a wakeup event!
 template <>
 void interrupt::handler<interrupt::irq::RTC_WKUP>() {
 	// Order is critical here.  Reversing these will give endless IRQs!
-	EXTI->PR1 = (1<<19); // clear rtc from exti pending
+	EXTI->PR1 = (1<<exti_rtc_wkup); // clear rtc from exti pending
 	RTC->ISR = ~(1<<10); // Clear WUTF (
-	ITM->stim_blocking(0, '$');
+	//ITM->stim_blocking(0, '$');
+	LPUART1->TDR = '$';
 }
 
+#if defined(STM32WB)
 template <>
-void interrupt::handler<interrupt::irq::EXTI1>() {
+void interrupt::handler<interrupt::irq::EXTI1>()
+#elif defined(STM32L4)
+template <>
+void interrupt::handler<interrupt::irq::EXTI15_10>()
+#else
+#error "unsupported platform"
+#endif
+{
 	EXTI->PR1 |= (1<<sw_3.n);
-	ITM->stim_blocking(0, '!');
+//	ITM->stim_blocking(0, '!');
 	pressed = true;
 }
-
