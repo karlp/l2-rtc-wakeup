4239a8fcb63331cf7652cbf4f698b17060df0fb9
Karl Palsson <karlp@tweak.net.au>
2022-02-07 17:31:47 +0000

    "main": l4, freertos button wakeup again.
    Now that we have lpuart, rtc, exti, bring all that back up into our
    freertos style, so we can now start working on the lptimer again.

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,14 @@
 #include <cal/cal.h>
 #include <cortex_m/debug.h>
 #include <dma/dma.h>
+#include <exti/exti.h>
 #include <gpio/gpio.h>
 #include <interrupt/interrupt.h>
 #include <pwr/pwr.h>
 #include <rcc/flash.h>
 #include <rcc/rcc.h>
 #include <rtc/rtc.h>
+#include <syscfg/syscfg.h>
 #include <timer/timer.h>
 #include <uart/uart.h>
 #include <usb/usb.h>
@@ -22,6 +24,7 @@
 auto led_r = GPIOB[1];
 auto led_g = GPIOB[0];
 auto led_b = GPIOB[5];
+#define HAS_LED_BLUE 1
 #elif defined(STM32F4)
 // Nucleo 144 boards at least...
 auto led_r = GPIOB[14];
@@ -32,19 +35,45 @@ auto led_b = GPIOB[7];
 auto led_r = GPIOE[9];
 auto led_g = GPIOE[15];
 auto led_b = GPIOE[12];
+#elif defined(STM32L4)
+auto led_g = GPIOA[5];
+auto sw_1 = GPIOC[13];
+#else
+#error "Platform unsupported"
 #endif
 
 /* disable this with a debugger if you want to measure power sanely
  * You'll then have to watch the ITM channels to see that things do what you think.
  */
-bool opt_really_use_leds = false;
+bool opt_really_use_leds = true;
+
+#include <cerrno>
+#include <cstdlib>
+#include <unistd.h>
+
+
+extern "C" int _write(int file, char* ptr, int len) {
+        int i;
+
+        if (file == STDOUT_FILENO || file == STDERR_FILENO) {
+                for (i = 0; i < len; i++) {
+                        if (ptr[i] == '\n') {
+				LPUART1.write_blocking('\r');
+                        }
+		LPUART1.write_blocking(ptr[i]);
+                }
+                return i;
+        }
+        errno = EIO;
+        return -1;
+}
+
 /**
  * Run at 32Mhz instead of laks "top speed" by default.
  * 32Mhz is what many ST WB demos run at, and if you don't need more, it's
  * probably a good choice.
  */
-const bool opt_clock_32mhz = true;
-
+#if defined(STM32WB)
 void krcc_init32(void) {
 	// Prefetch and both caches, plus 1WS for 32MHz
 	FLASH->ACR = 0x700 | 1;
@@ -53,121 +82,121 @@ void krcc_init32(void) {
 	RCC->CR |= (1<<16);
 	while(!(RCC->CR & (1<<17)));
 
-	/* I initially setup PLL to 32Mhz, because I had PLL to 64, and just downtuned
-	 * but if you want lower power, use HSE by itself at 32.  The PLL is useless burn
-	 */
-#if defined(USE_PLL_32)
-	// Configure and enable PLL.
-	// R=2, Q = 2, P = 2, M = 2, N = 8, src=HSE
-	const auto m = 2;
-	const auto n = 8;
-	const auto p = 2;
-	const auto q = 2;
-	const auto r = 4;
-	//const auto hse_pre_div2 = false;
+	/* No PLL, that just wastes power! */
 
-	RCC->PLLCFGR = ((r-1)<<29) | (1<<28) | ((q-1)<<25) | ((p-1)<<17) | (n<<8) | ((m-1)<<4) | (3<<0);
-	RCC->CR |= (1<<24);
-	while(!(RCC->CR & (1<<25)));
-
-	// Switch to PLL.
-	RCC->CFGR |= 0x3;
-	while((RCC->CFGR & (3 << 2)) != (3 << 2)); // SWS = PLL
-#else
-	//RCC->CFGR &= ~0x3; // do I need this? unlikely at reset? possibly at other times?
+	// Make sure to clear bits, we may be waking up!
+	RCC->CFGR &= ~0x3;
 	RCC->CFGR |= 0x2;
-	while ((RCC->CFGR & (2<<2)) != (2<<2)); // SWS = HSE
-#endif
+	while ((RCC->CFGR & (3<<2)) != (2<<2)); // SWS = HSE
 	// Leave prescalers alone...
 }
+#endif
 
-/** We're going to use this for low power wakeups! see l_freertos.cpp....  (code organization wat?
- * Big difference to ST implementation, we're _ONLY_ going to use it for low power sleep wakeups
- * Because we're _only_ supporting freertos, we don't need to have linked lists
- * of overlapping timers all using this rtc wakeup timer.  just use the os timers for that,
- * and it actually turns into the same thing, handled internally.
- * this makes it _wayyyyyy simpler_ but also much less "flexible"
- */
-static void krtc_init(void) {
-	PWR->CR1 |= (1<<8); // Unlock backup domain
-	
-	//FUcking wat? (this is st style)
-//	if (!(RCC->BDCR & (1<<1))) { // LSERDY
-		// Force a backup domain reset...
-		RCC->BDCR |= (1<<16);
-		RCC->BDCR &= ~(1<<16);
-		RCC->BDCR |= (1<<0); // LSE on...
+#if defined(STM32L4)
+static void krcc_init32(void) {
+	// We're just goign to use MSI at 32Mhz, as it's "easy" to setup,
+	// and it doesn't matter what your crystal is.  (Even if nucleos have 8Mhz)
 
-		while (!(RCC->BDCR & (1<<1))) {
-			; // LSE RDY
-		}
 
-		
-		RCC->BDCR |= (1<<8); // RTCSEL = LSE
-		
+	// Prefetch and both caches, plus 1WS for 32MHz
+	FLASH->ACR = 0x700 | 1;
+
+	// Enable MSI (it should already be on, it's the reset clock)
+	RCC->CR |= (1<<3) | 1; // Also enable range selection here
+	// Can't modify MSI range when it's on and not ready
+	while (!(RCC->CR & (1 << 1)));
+	uint32_t msi_range = 10; // 32Mhz
+	RCC->CR &= ~(0xf<<4);
+	RCC->CR |= (msi_range << 4);
+
+	RCC->CFGR &= ~(0x3<<0); // Set MSI as clock source
+	// L4 Needs these to be enabled first.
+	RCC.enable(rcc::PWR);
+	RCC.enable(rcc::SYSCFG);
+}
+#endif
+
+
+
+static void krtc_init(void)
+{
+	// always need that even if RTC itself is already setup
+	PWR->CR1 |= (1 << 8); // Unlock backup domain
+
+	// Use the last backup register as a flag that we're already initialized
+	if (RTC->BKP[19] == 0xcafe) {
+		printf("RTC: already setup!\n");
+		return;
+	}
+
+	// We've decided that it's "not setup" so go ahead and hard reset
+	// The RTC.  You want to be careful with this, or you lose your
+	// precious calendar.
+	printf("hard reset backup domain\n");
+	RCC->BDCR |= (1 << 16);
+	RCC->BDCR &= ~(1 << 16);
+
+	// make sure LSE is on, we need that for most sane uses of RTC
+	RCC->BDCR |= 1;
+	while (!(RCC->BDCR & (1 << 1))) {
+		; // wait for LSERDY
+	}
+
+	RCC->BDCR |= (1 << 8); // RTCSEL == LSE
+	RCC->BDCR |= (1 << 15); // RTCEN
+	RCC.enable(rcc::RTCAPB);
+	// WB simply does a single read here, and L4 just hangs
+//	while (!(RCC->APB1ENR1 & rcc::RTCAPB)) {
+//		; // make sure we have access!
 //	}
-	
-	
-////	RCC->BDCR = (1<<15) | (1<<8) | (1<<0); // RTCEN | RTCSEL=LSE | LSE ON
-//	// no difference splitting like ST does...
-//	RCC->BDCR |= (1<<8); // RTCSEL = LSE
-//	RCC->BDCR |= (1<<15); // RTCEN
-//	RCC.enable(rcc::RTCAPB);  // APB access, apparently important too...
-//
-////	RTC->CR |= (1<<5); // Bypass shadow ? why do we care?
-	
-	// Now, MX_RTC_Init()
-	RCC->BDCR |= (1<<15); // RTCEN
-	RCC.enable(rcc::RTCAPB);  // ST has an error in this, but, whatever, obviously not very important
-	
-	// Ok, now rtc itself..
-	RTC->WPR = 0xCA;
-	RTC->WPR = 0x53;
-//	RTC.unlock();
-	
-	
-	//// Might as well init calendar? 
-	//RTC->ISR |= (1<<7); // set init
-	RTC->ISR = 0xffffffff; // ST style?!
-	while (!(RTC->ISR & (1<<6))) {   // FIXME - this always hangs....
-		; // Poll til ready.
+
+	RTC.unlock();
+
+	RTC->PRER = 0x7f00ff; // default, gives 1sec from 32768
+
+	// setup calendar as well
+	RTC->ISR = (1 << 7);
+	while (!(RTC->ISR & (1 << 6))) {
+		; // Wait for INITF
 	}
-	RTC->PRER = 0x7f00ff;  // yes, default is divided by (0x7f+1)*(0xff+1) => 32768....
-	
-	// zero time is as good as any....
-	RTC->TR = 0;
-	RTC->DR = 0;
-	
-	RTC->CR &= ~(1<<6);  // 24 hour yo
-	RTC->ISR &= ~(1<<7);  // Clear init to start calendar
-	///// End of "calendar"
-	
-	//// WAKEUP TIMER _init_ but don't start...
-	// WUCKSEL = 16 is good...
-	RTC->CR &= ~(1<<10); // clear WUTE while we setup
-	while (!(RTC->ISR & (1<<2))) {
-		; // Wait for WUTWF
-	}
-	RTC->CR &= ~(0x7<<0);  // Clear WUCKSEL
-	RTC->CR |= (0<<0); // WUCKSEL = RTC/16...
-	// I _think_ we can start it here, but jsut don't turn on the interrupt?
-	RTC->CR |= (1<<10); // WUTE
-	// But WUTR is protected by WUTWF anyway, so probably doesn't buy us much...
-	
-	
-//	RTC.lock();
-	RTC->WPR=0xff;
-	
-	
-	// Oh yeah, someone has to allow the RTC wakeup timer irq in nvic to make it wakeu right?
-	NVIC.enable(interrupt::irq::RTC_WKUP);
+	RTC->CR &= ~(1 << 6); // 24hr format.
+	RTC->DR = 0x220127 | (4 << 13); // Thursday,2022-01-27.
+	RTC->TR = 0x092042; // 09:20:42 AM
+	RTC->ISR &= ~(1 << 7); // Clear init, start calendar
+
+	// set wakekup clock sel to rtc/16, so ~488usecs per tick
+	RTC->CR &= ~(0x7 << 0);
+	RTC->CR |= (0 << 0);
+
+	RTC.lock();
+
+	RTC->BKP[19] = 0xcafe;
 }
 
-static TimerHandle_t xBlueTimer;
-static TaskHandle_t th_blue;
-static void prvTimerBlue(TimerHandle_t xTimer)
-{
-	xTaskNotifyGive(th_blue);
+static void kexti_init(void) {
+	// sw_3.. is D1...
+#if defined(STM32WB)
+	RCC.enable(rcc::GPIOD);
+#elif defined(STM32L4)
+	RCC.enable(rcc::GPIOC);
+#else
+#error "Unhandled platform, check your switch/led ports"
+#endif
+
+	sw_1.set_mode(Pin::Mode::Input);
+	sw_1.set_pull(Pin::Pull::PullUp);
+
+	SYSCFG->EXTICR[sw_1.n >> 2] &= ~(0xf << ((sw_1.n & 0x3) * 4));
+	SYSCFG->EXTICR[sw_1.n >> 2] |= (sw_1.get_portnum() << ((sw_1.n & 0x3) * 4));
+
+	EXTI->IMR1 |= (1 << sw_1.n); // unmask
+	EXTI->FTSR1 |= (1 << sw_1.n); // falling edge
+
+#if defined(STM32WB)
+	NVIC.enable(interrupt::irq::EXTI1);
+#elif defined(STM32L4)
+	NVIC.enable(interrupt::irq::EXTI15_10);
+#endif
 }
 
 /**
@@ -177,7 +206,16 @@ static void prvTimerBlue(TimerHandle_t xTimer)
 static void prvTaskBlinkGreen(void *pvParameters)
 {
 	(void)pvParameters;
-	led_g.set_mode(Pin::Output);
+	if (opt_really_use_leds) {
+#if defined(STM32L4)
+		RCC.enable(rcc::GPIOA);
+#elif defined(STM32WB)
+		RCC.enable(rcc::GPIOB);
+#else
+#warning "no leds configured..."
+#endif
+		led_g.set_mode(Pin::Output);
+	}
 
 	int i = 0;
 	while (1) {
@@ -191,6 +229,13 @@ static void prvTaskBlinkGreen(void *pvParameters)
 	}
 }
 
+#if defined(HAS_LED_BLUE)
+static TimerHandle_t xBlueTimer;
+static TaskHandle_t th_blue;
+static void prvTimerBlue(TimerHandle_t xTimer)
+{
+	xTaskNotifyGive(th_blue);
+}
 /**
  * Blue uses a timer to notify the task it's time to do the blink.
  * @param pvParameters
@@ -210,26 +255,106 @@ static void prvTaskBlinkBlue(void *pvParameters) {
 		printf("B: %d\n", i);
 	}
 }
+#endif
+
+static void klpuart_init(void) {
+//	RCC.enable(rcc::USART1);
+	RCC.enable(rcc::LPUART1);
+
+	// sysclock? lpuart1
+	RCC->CCIPR &= ~(0x3<<10);
+	RCC->CCIPR |= (1<<10);
+	// usart1
+	RCC->CCIPR &= ~(0x3<<0);
+	RCC->CCIPR |= (1<<0);
+
+
+#if defined(STM32WB)
+	// PA2,3 for lpuart1
+	RCC.enable(rcc::GPIOA);
+	auto lp1_rx = GPIOA[2];
+	auto lp1_tx = GPIOA[3];
+#elif defined(STM32L4)
+	RCC.enable(rcc::GPIOC);
+	auto lp1_rx = GPIOC[0];
+	auto lp1_tx = GPIOC[1];
+#endif
+	lp1_rx.set_af(8);
+	lp1_tx.set_af(8);
+	lp1_rx.set_mode(Pin::Mode::AF);
+	lp1_tx.set_mode(Pin::Mode::AF);
+
+//	RCC.enable(rcc::GPIOB);
+//	auto pb6 = GPIOB[6];
+//	auto pb7 = GPIOB[7];
+//	pb6.set_af(7);
+//	pb7.set_af(7);
+//	pb6.set_mode(Pin::Mode::AF);
+//	pb7.set_mode(Pin::Mode::AF);
+//
+//	USART1->CR1 = (1<<29); // fifo, 8bit, no parity, no special
+//	//USART1->CR1 = 0;
+//	USART1->CR2 = 0; // standard
+//	// this is for lpuart USART1->BRR = (256*32000000u / 115200); // 71111?
+//	USART1->BRR = 32000000u / 115200u;
+//	USART1->CR1 |= (1<<3)|1; // TX only, enabled
+
+	LPUART1->CR1 = (1<<29); // fifo, 8bit, no parity, no special
+	//LPUART1->CR1 = 0;
+	LPUART1->CR2 = 0; // standard
+	LPUART1->BRR = (32000000u / 115200) * 256; // careful with overflows!
+	LPUART1->CR1 |= (1<<3)|1; // TX only, enabled
+
+}
+
+static void print_date(void) {
+	// order of read is important!
+	uint32_t s = RTC->SSR;
+	uint32_t t = RTC->TR;
+	uint32_t d = RTC->DR;
+
+	printf("20%02lx:%02lx:%02lx T %02lx:%02lx:%02lx.%03ld\n",
+		(d >> 16) & 0xff,
+		(d >> 8) & 0x1f,
+		d & 0x3f,
+		(t >> 16) & 0x3f,
+		(t >> 8) & 0x7f,
+		t & 0x7f,
+		(1000*(255 - s)/256) // based on "default" fck_spre
+		);
+}
+
+static TaskHandle_t th_button;
+void prvTaskButton(void *pvParameters) {
+	(void)pvParameters;
+
+	int i = 0;
+	while (1) {
+		i++;
+		BaseType_t rc = xTaskNotifyWait(1, 1, NULL, pdMS_TO_TICKS(3500));
+		if (rc) {
+			printf("Woke via button press!\n");
+		}
+		print_date();
+	}
+}
 
 
 int main() {
-	if (opt_clock_32mhz) {
-		krcc_init32();
-	} else {		
-		rcc_init();
-	}
-	printf("clocked up, turning on rtc...\n");
+	krcc_init32();
+	klpuart_init();
+	printf("Started from scratch, clocked up\n");
 
 	// Turn on DWT_CYCNT.  We'll use it ourselves, and pc sampling needs it too.
 	DWT->CTRL |= 1;
-	
-	krtc_init();
-	printf("RTC init, ready to start!\n");
 
-	RCC.enable(rcc::GPIOB);
-	RCC.enable(rcc::GPIOE);
+	krtc_init();
+	printf("RTC init, ready to  start!\n");
+
+	kexti_init();
 
 	xTaskCreate(prvTaskBlinkGreen, "blink.green", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1, NULL);
+#if defined(HAS_LED_BLUE)
 	xTaskCreate(prvTaskBlinkBlue, "blink.blue", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1, &th_blue);
 
 	xBlueTimer = xTimerCreate("blue.blink", 200 * portTICK_PERIOD_MS, true, 0, prvTimerBlue);
@@ -242,9 +367,14 @@ int main() {
 	} else {
 		// boooo!!!!! fixme trace?
 	}
+#endif
+
+	xTaskCreate(prvTaskButton, "buttons", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1, &th_button);
 
 	// Required to use FreeRTOS ISR methods!
-//	NVIC.set_priority(interrupt::irq::DMA1_CH1, 6<<configPRIO_BITS);
+	// ( set the priority for all of them, regardless of platform...
+	NVIC.set_priority(interrupt::irq::EXTI1, 6<<configPRIO_BITS);
+	NVIC.set_priority(interrupt::irq::EXTI15_10, 6<<configPRIO_BITS);
 
 	vTaskStartScheduler();
 
@@ -272,4 +402,19 @@ template <>
 void interrupt::handler<interrupt::exception::SysTick>() {
 	xPortSysTickHandler();
 }
+
+#if defined(STM32WB)
+template <>
+void interrupt::handler<interrupt::irq::EXTI1>()
+#elif defined(STM32L4)
+template <>
+void interrupt::handler<interrupt::irq::EXTI15_10>()
+#else
+#error "unsupported platform"
+#endif
+{
+	EXTI->PR1 |= (1<<sw_1.n);
+	vTaskNotifyGiveFromISR(th_button, NULL);
+}
+
 
