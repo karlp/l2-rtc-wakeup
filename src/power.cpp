1059b3033916ca2d27fa74a9a5985dfede4011c0
Karl Palsson <karlp@tweak.net.au>
2022-02-10 13:56:02 +0000

    stop2 working on wb and l4!
    about time.
    
    need to a) properly remove jumpers both for vdd and swdio on the level
    translator
    b) make sure to ask c2 to stop.
    
    Now: stop2, via lptim tickless, while idle
      WB: ~3uA
      L4: ~27uA
    
    That's "good" (if anything, the l4 is too high for it's own datasheet)
    
    Now: lpsleep(range0), via lptim tickless, while idle
      WB: ~400uA and jittery  (much too high!)
      L4: ~57uA  (fine...)
    
    We'll work on that next i guess?

--- src/power.cpp
+++ src/power.cpp
@@ -5,6 +5,8 @@
 #include <stdio.h>
 #include "FreeRTOS.h"
 
+#include <exti/exti.h>
+#include <gpio/gpio.h>
 #include <interrupt/interrupt.h>
 #include <pwr/pwr.h>
 #include <rcc/rcc.h>
@@ -14,10 +16,28 @@ static uint32_t _rcc_cr;
 static uint32_t _rcc_cfgr;
 auto const _msi_range = 0;
 
+// hackyhack..
+extern Pin led_g;
+
 void pre_lpsleep(uint32_t _msi_range)
 {
 	_rcc_cr = RCC->CR;
 	_rcc_cfgr = RCC->CFGR;
+#if defined(STM32WB)
+	// According to code samples, but not clearly described
+	// But not clearly described in the ref man, this call would normally be
+	// made by the CPU2 code itself.  Without turning it on and running,
+	// we do it ourselves...
+	// We can actually set it _lower_ than the cpu1 mode, but this is sufficient...
+	// Some single core demos set this up front, once, but we can do it here easily rightnow.
+	PWR.set_lpms_c2(4);  // use shutdown all the way?  (no, makes no difference here :(
+#endif
+
+//#if defined(STM32WB) // This has no impact on this mode!
+//	// Separate section for errata on stop mode and debug
+//	EXTI->IMR2 &= ~(1<<(48-32));
+//	EXTI->C2IMR2 &= ~(1<<(48-32));
+//#endif
 	// Ensure MSI is on, and switch down to that range...
 	RCC->CR |= (1 << 3) | 1; // Also enable range selection here
 	// Can't modify MSI range when it's on and not ready
@@ -48,12 +68,47 @@ void pre_stop(uint32_t stop_mode)
 	//		printf("stop pre %lu\n", _mode);
 	_rcc_cr = RCC->CR;
 	_rcc_cfgr = RCC->CFGR;
+#if defined(STM32WB)
+	// According to code samples, but not clearly described
+	// But not clearly described in the ref man, this call would normally be
+	// made by the CPU2 code itself.  Without turning it on and running,
+	// we do it ourselves...
+	// We can actually set it _lower_ than the cpu1 mode, but this is sufficient...
+	// Some single core demos set this up front, once, but we can do it here easily rightnow.
+	PWR.set_lpms_c2(stop_mode);
+#endif
+
+#if 0  // TODO measure this? (but need to then restore properly for lpuart and leds and shit?
+	led_g.off();
+	// This section is in some demos, we're doing it for now just to try for completeness
+	RCC.enable(rcc::GPIOA);
+	RCC.enable(rcc::GPIOB);
+	RCC.enable(rcc::GPIOC);
+	RCC.enable(rcc::GPIOD);
+	GPIOA->MODER = 0xffffffff;
+	GPIOB->MODER = 0xffffffff;
+	GPIOC->MODER = 0xffffffff;
+	GPIOD->MODER = 0xffffffff;
+	GPIOA->PUPDR = 0;
+	GPIOB->PUPDR = 0;
+	GPIOC->PUPDR = 0;
+	GPIOD->PUPDR = 0;
+	RCC.disable(rcc::GPIOA);
+	RCC.disable(rcc::GPIOB);
+	RCC.disable(rcc::GPIOC);
+	RCC.disable(rcc::GPIOD);
+#endif
+
+	// This doesn't _seem_ to matter if you unplug the debugger,
+	// and doesn't help even if you leave it in, but it's in the errata,
+	// so maybe I just got lucky in my testsm, better leave it in.
+#if defined(STM32WB) && 1
+	// Separate section for errata on stop mode and debug
+	EXTI->IMR2 &= ~(1<<(48-32));
+	EXTI->C2IMR2 &= ~(1<<(48-32));
+#endif
+	
 	PWR.set_lpms(stop_mode);
-	//		PWR.set_lpms_c2(_mode);
-	//		if ((PWR->EXTSCR >> 8) & 0xf) {
-	//			// if any of thos fucking flags is set, clear them all!
-	//			PWR->EXTSCR |= 0x3;
-	//		}
 	// Enable SLEEPDEEP
 	SCB->SCR |= (1 << 2);
 }
@@ -72,13 +127,13 @@ extern "C" {
 	void myPreSleepFunction(TickType_t xModifiableIdleTime)
 	{
 		while (!(LPUART1->ISR & (1<<6)));
-		pre_lpsleep(0);
-//		pre_stop(2);
+//		pre_lpsleep(0);
+		pre_stop(2);
 	}
 
 	void myPostSleepFunction(const TickType_t xExpectedIdleTime)
 	{
-		post_lpsleep();
-//		post_stop();
+//		post_lpsleep();
+		post_stop();
 	}
 }
